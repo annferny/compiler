05d77466dbea18fae9f50949d395c020759fc59d
Anna Gavrilova <anna.gavrilova@stud.htw-dresden.de>
2024-11-28 11:16:31 +0100

    parser v2

--- beleg/parser.c
+++ beleg/parser.c
@@ -3,7 +3,14 @@
 
 typedef unsigned long ul;
 
+tBog gStatement[], gBlock[], gTerm[], gFactor[], gCondition[];
 
+tBog gProgramm[] =
+{
+	/* 0*/ {BgGr, {.G = gBlock}, NULL, 1, 0}, /*(0)---block--->(1)*/ // NULL steht jetzt für zukunftige Funktionen
+    /* 1*/ {BgSy, {(ul)'.'},     NULL, 2, 0}, /*(1)---'.'--->(E)*/
+    /* 2*/ {BgEn, {(ul)0},       NULL, 0, 0}  /*(E)--------(ENDE) */
+};
 
 tBog gBlock[] =
 {
@@ -21,24 +28,59 @@ tBog gBlock[] =
     /*11*/ {BgMo, {(ul)morphemeCodeIdentifier}, NULL,12, 0}, /*(11)---ident--->(12)*/
     /*12*/ {BgSy, {(ul)';'},                    NULL,13, 0}, /*(12)----';'---->(13)*/
     /*13*/ {BgGr, {.G = gBlock},                NULL,14, 0}, /*(13)---block--->(14)*/
-    /* 9*/ {BgSy, {(ul)';'},                    NULL,10, 0}, /*(9)----';'----->(10)*/
-
+    /*14*/ {BgSy, {(ul)';'},                    NULL,10, 0}, /*(14)----';'---->(10)*/
+    /*15*/ {BgGr, {.G = gStatement},            NULL,16, 0}, /*(15)---block--->(16)*/
+    /*16*/ {BgEn, {(ul)0},                      NULL, 0, 0}  /*(E)--------(ENDE) */
 };
 
-tBog gProgramm[] =
+tBog gExpression[] = 
 {
-	/* 0*/ {BgGr, {.G = gBlock}, NULL, 1, 0}, /*(0)---block--->(1)*/ // NULL steht jetzt für zukunftige Funktionen
-    /* 1*/ {BgSy, {(ul)'.'},     NULL, 2, 0}, /*(1)---'.'--->(E)*/
-    /* 2*/ {BgEn, {(ul)0},       NULL, 0, 0}  /*(E)--------(ENDE) */
+    /* 0*/ {BgSy, {(ul)'-'},                    NULL, 1, 2}, /*(0)----'-'----->(1)*/
+    /* 1*/ {BgGr, {.G = gTerm},                 NULL, 3, 0}, /*(1)---term----->(3)*/
+    /* 2*/ {BgGr, {.G = gTerm},                 NULL, 3, 0}, /*(2)---term----->(3)*/
+    /* 3*/ {BgNl, {.S = 0 },                    NULL, 4, 0}, /*(3)---Nil------>(4)*/
+    /* 4*/ {BgSy, {.S = '+'},                   NULL, 5, 6}, /*(4)----'+'----->(5)*/
+    /* 5*/ {BgGr, {.G = gTerm},                 NULL, 3, 0}, /*(5)---term----->(3)*/
+    /* 6*/ {BgSy, {.S = '-'},                   NULL, 7, 8}, /*(6)----'-'----->(7)*/
+    /* 7*/ {BgGr, {.G = gTerm},                 NULL, 3, 0}, /*(7)---term----->(3)*/
+    /* 8*/ {BgEn, {.S = 0},                     NULL, 0, 0}  /*(E)----------(ENDE)*/
+};
+
+tBog gTerm[] =
+{
+    /* 0*/ {BgGr, {.G = gFactor},               NULL, 1, 0}, /*(0)---factor--->(1)*/
+    /* 1*/ {BgNl, {.S = 0 },                    NULL, 2, 0}, /*(1)---Nil------>(2)*/
+    /* 2*/ {BgSy, {.S = '*'},                   NULL, 3, 8}, /*(2)----'*'----->(3)*/
+    /* 3*/ {BgGr, {.G = gFactor},               NULL, 1, 0}, /*(3)---factor--->(1)*/
+    /* 4*/ {BgSy, {.S = '/'},                   NULL, 5, 6}, /*(4)----'*'----->(5)*/
+    /* 5*/ {BgGr, {.G = gFactor},               NULL, 1, 0}, /*(5)---factor--->(1)*/
+    /* 6*/ {BgEn, {.S = 0},                     NULL, 0, 0}  /*(E)----------(ENDE)*/
+};
+
+tBog gStatement[] =
+{
+    /* 0*/ {BgMo, {.M = morphemeCodeIdentifier},NULL, 1, 3}, /*(0)---ident---->(1)*/
+    /* 1*/ {BgSy, {(ul)zResult},                NULL, 2, 0}, /*(1)----':='---->(2)*/
+    /* 2*/ {BgGr, {.G = gExpression},           NULL, , 0}, /*(2)---express->()*/
+    /* 3*/ {BgSy, {(ul)zIf,                     NULL, 4, 7}, /*(3)----'if'---->(4)*/
+    /* 4*/ {BgGr, {.G = gCondition},            NULL, 5, 0}, /*(4)--condition->(5)*/
+    /* 5*/ {BgSy, {(ul)zThen,                   NULL, 6, 0}, /*(5)---'then'--->(6)*/
+    /* 6*/ {BgGr, {.G = gStatement},            NULL,, 0}, /*(6)---statement--->()*/
+    /* 7*/ {BgSy, {(ul)zWhile,                  NULL, 8,11}, /*(7)---'while'--->(8)*/
+    /* 8*/ {BgGr, {.G = gCondition},            NULL, 9, 0}, /*(8)--condition->(9)*/
+    /* 9*/ {BgSy, {(ul)zDo,                     NULL, 8, 0}, /*(7)---'while'--->(8)*/
+    /*10*/ {BgGr, {.G = gStatement},            NULL,, 0}, /*(10)---statement--->()*/
+    /*11*/ {BgSy, {(ul)zBegin,                  NULL,12,14}, /*(11)---'begin'--->(12)*/
+    /*12*/ {BgGr, {.G = gStatement},            NULL,13,}, /*(12)--statement-->(13)*/
+    /*13*/ {BgSy, {.S = ';'},                   NULL,12, 0}, /*(13)----'*'---->(12)*/
 };
 
 tBog gFactor[] =
 {
-        /* 0*/ {BgMo, {(ul)morphemeCodeIdentifier}, NULL, 5, 1}, /*(0)---ident--->(E)*/ // NULL steht jetzt für zukunftige Funktionen
-        /* 1*/ {BgMo, {(ul)morphemeCodeNumber},     NULL, 5, 2}, /* +---number--->(E)*/
-        /* 2*/ {BgSy, {(ul)'('},                    NULL, 3, 0}, /*(+)----'('---->(3)*/
-        /* 3*/ {BgGr, {.G = gBlock},                NULL, 4, 0}, /*(1)---express->(4)*/
-        /* 4*/ {BgSy, {(ul)')'},                    NULL, 5, 0}, /*(0)----')'---->(E)*/
-        /* 5*/ {BgEn, {(ul)0},                      NULL, 0, 0}  /*(E)--------(ENDE) */
+    /* 0*/ {BgMo, {(ul)morphemeCodeIdentifier}, NULL, 5, 1}, /*(0)---ident--->(E)*/ // NULL steht jetzt für zukunftige Funktionen
+    /* 1*/ {BgMo, {(ul)morphemeCodeNumber},     NULL, 5, 2}, /* +---number--->(E)*/
+    /* 2*/ {BgSy, {(ul)'('},                    NULL, 3, 0}, /*(+)----'('---->(3)*/
+    /* 3*/ {BgGr, {.G = gExpression},           NULL, 4, 0}, /*(1)---express->(4)*/
+    /* 4*/ {BgSy, {(ul)')'},                    NULL, 5, 0}, /*(0)----')'---->(E)*/
+    /* 5*/ {BgEn, {(ul)0},                      NULL, 0, 0}  /*(E)--------(ENDE) */
 };
-
